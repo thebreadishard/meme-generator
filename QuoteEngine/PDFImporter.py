3ee76bf439b52d0345e87ff309ca6e97c6f80a9f
thebreadishard <112763215+thebreadishard@users.noreply.github.com>
2022-10-01 10:10:35 +0200

    PDFImporter.py

---
+++ QuoteEngine/PDFImporter.py
@@ -0,0 +1,32 @@
+from typing import List
+import subprocess
+import os
+import random
+
+from .IngestorInterface import IngestorInterface
+from .QuoteModel import QuoteModel
+
+
+class PDFImporter(IngestorInterface):
+    allowed_extensions = ['pdf']
+
+    @classmethod
+    def parse(cls, path: str) -> List[QuoteModel]:
+        if not cls.can_ingest(path):
+            raise Exception('cannot ingest exception')
+
+        tmp = f'./tmp/{random.randint(0, 1000000)}.txt'
+        call = subprocess.call(['pdftotext', path, tmp])
+
+        file_ref = open(tmp, "r")
+        quotes = []
+        for line in file_ref.readlines():
+            line = line.strip('\n\r').strip()
+            if len(line) > 0:
+                parse = line.split(' - ')
+                new_quote = QuoteModel(parse[0], parse[1])
+                quotes.append(new_quote)
+
+        file_ref.close()
+        os.remove(tmp)
+        return quotes
