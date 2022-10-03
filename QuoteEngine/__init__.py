ae4e5b248dbf22de14488908c66aa1ba2c73956f
thebreadishard <112763215+thebreadishard@users.noreply.github.com>
2022-10-03 16:39:57 +0200

    Init.py

--- QuoteEngine/__init__.py
+++ QuoteEngine/__init__.py
@@ -1,4 +1,6 @@
 from .DocxIngestor import DocxIngestor
 from .CSVIngestor import CSVIngestor
-from .TXTIngestor import TextIngestor
+from .TextIngestor import TextIngestor
 from .PDFIngestor import PDFIngestor
+from .Ingestor import Ingestor
+from .QuoteEngine import IngestorInterface
