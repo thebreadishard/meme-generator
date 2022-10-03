e748f9f06f97161054805577a310416907c2e974
thebreadishard <112763215+thebreadishard@users.noreply.github.com>
2022-10-03 16:38:07 +0200

    Ingestor.py

---
+++ QuoteEngine/Ingestor.py
@@ -0,0 +1,17 @@
+from typing import List
+
+from .QuoteEngine import IngestorInterface
+from .QuoteModel import QuoteModel
+from .DocxIngestor import DocxIngestor
+from .CSVIngestor import CSVIngestor
+from .PDFIngestor import PDFIngestor
+from .TextIngestor import TextIngestor
+
+class Ingestor(IngestorInterface):
+    importers = [DocxIngestor, CSVIngestor, PDFIngestor, TextIngestor]
+
+    @classmethod
+    def parse(cls, path: str) -> List[QuoteModel]:
+        for importer in cls.importers:
+            if importer.can_ingest(path):
+                return importer.parse(path)
