5bde1f8f17c83e2cbb54aba3552f4f2956a27d5b
thebreadishard <112763215+thebreadishard@users.noreply.github.com>
2022-10-08 16:35:38 +0200

    Grammar error fix

--- meme.py
+++ meme.py
@@ -8,7 +8,7 @@ from MemeEngine import MemeEngine
 
 
 def generate_meme(path=None, body=None, author=None):
-    """ Generate a meme given an path and a quote """
+    """ Generate a meme given a path and a quote """
     img = None
     quote = None
 
