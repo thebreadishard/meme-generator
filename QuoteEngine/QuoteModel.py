a96d88e1e97cc2cb913d02ee887a3a4cce03362f
thebreadishard <112763215+thebreadishard@users.noreply.github.com>
2022-10-08 14:26:14 +0200

    Updated QuoteModel.py

--- QuoteEngine/QuoteModel.py
+++ QuoteEngine/QuoteModel.py
@@ -1,17 +1,11 @@
 class QuoteModel:
     def __init__(self, body, author):
-        self.body = body
-        self.author = author
-
-    def __repr__(self):
         """Create a new Quote.
         :param body: a str text, the message of the quote.
         :param author: a str text, the author of the quote.
         """
-        return f'{self.body} - {self.author}'
+        self.body = body
+        self.author = author
 
     def __repr__(self):
-        """Return `repr(self)`.
-        a computer-readable string representation of this object.
-        """
-        return f'<{self.body}, {self.author}>'
+        return f'{self.body} - {self.author}'
